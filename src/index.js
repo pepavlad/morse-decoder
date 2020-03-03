6047205fbf06bc73c34cddf534c0f9829fa909d2
pepavlad <pepelvlad@gmail.com>
2020-03-03 21:02:31 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = [];
+    let arrStr = [];
+    expr.split('');
+    for (let i = 0; i < expr.length / 10; i++) {
+        arrStr[i] = expr.slice((i * 10), (i * 10) + 10);
+    }
+    for (let i = 0; i < arrStr.length; i++) {
+        result += convertToStr(arrStr[i].split(''));
+    }
+    function convertToStr(arrStr){
+        let subStr = "";
+        arrStr.forEach(element => subStr += element);
+        if (subStr == "**********") {
+            return (" ");
+        }
+        let morseCode = "";
+        let numbs = "";
+        for (i = 0; i < 10; i += 2) {
+            numbs = ""
+            numbs = subStr[i] + subStr[i + 1];
+            if (numbs == "10") {
+               morseCode += ".";
+            }
+            if (numbs == "11") {
+                morseCode += "-";
+            }
+        }
+        return (MORSE_TABLE[morseCode]);
+    }
+    return result;
 }
 
 module.exports = {
